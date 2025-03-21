97077e23090ac1fcc87e21620129aa2f219e6c5b
wcenatus <wcenatus@gmail.com>
2025-03-21 03:19:48 -0400

    Fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
-import { DogSearch } from "./components/integrated/dog-search";
-import { Login } from "./components/simple/login/login";
-import { useAuth } from "./context/auth-context";
+import { DogSearch } from "@/components/integrated/dog-search";
+import { Login } from "@/components/integrated/login";
+import { useAuth } from "@/context/auth-context";
 function App() {
   const { isAuthenticated } = useAuth();
   useEffect(() => {
